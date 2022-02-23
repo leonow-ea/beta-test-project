9cc917b5ebacde82b597a4df17c400f5884be26e
leonow-ea <100294315+leonow-ea@users.noreply.github.com>
2022-02-23 20:17:32 +0300

    Add files via upload

---
+++ newphp/application/core/view.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\core;
+
+class View
+{
+    public $template_view;
+
+    function generate($content_view, $template_view, $data = null)
+    {
+        if (is_array($data)) {
+            extract($data);
+        }
+
+        include '../application/views/' . $template_view;
+    }
+}
